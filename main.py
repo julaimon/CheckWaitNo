8734a0ca2c45fcc688e782c561a9a186912fe784
Julaimin <julaimon@hotmail.com>
2024-07-18 11:32:45 +0800

    add

--- main.py
+++ main.py
@@ -10,12 +10,15 @@ doc_list=gd.get_current_data(url2)
 #print(r1.text.split(",,"))
 
 #print(no_raw)
+message_list = ""
 for i in no_raw.split(';'):
     for j in [i.split('-')]:
         room_no=j[1]
         curr_no=j[2]
         update_time=j[5]
-        print("目前"+room_no+"診看的號碼是:"+ curr_no +" 更新時間:" +update_time)
+        message_list += "目前"+room_no+"診看的號碼是:"+ curr_no +" 更新時間:" +update_time
+        #print("目前"+room_no+"診看的號碼是:"+ curr_no +" 更新時間:" +update_time)
+print(message_list)
 
 for i in doc_list.split(',,'):
     if len (i.split(',')) == 3:
